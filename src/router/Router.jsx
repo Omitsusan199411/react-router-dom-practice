e58b71445eacf2bde041ce21ebfbc3e4ab059fa2
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-05-18 02:07:25 +0900

    404ページ遷移 一致しないパスを404ページに遷移させる

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
 import { page2Routes } from "./Page2Routes";
 import { page1Routes } from "./Page1Routes";
+import { Page404 } from "../Page404";
 
 export const Router = () => {
   return (
@@ -41,6 +42,10 @@ export const Router = () => {
           </Switch>
         )}
       />
+      {/* どのパスにも一致しない場合は404遷移 */}
+      <Route path="*">
+        <Page404 />
+      </Route>
     </Switch>
   );
 };
