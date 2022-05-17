1af2875290fd23e712d6f88b6b0cd169efea6476
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-05-18 02:12:25 +0900

    Page1のネストパスに404ページのルート設定

--- src/router/Page1Routes.jsx
+++ src/router/Page1Routes.jsx
@@ -1,6 +1,7 @@
 import { Page1 } from "../Page1";
 import { Page1DetailA } from "../Page1DetailA";
 import { Page1DetailB } from "../Page1DetailB";
+import { Page404 } from "../Page404";
 
 export const page1Routes = [
   {
@@ -17,5 +18,10 @@ export const page1Routes = [
     path: "/detailB",
     exact: false,
     children: <Page1DetailB />
+  },
+  {
+    path: "*",
+    exact: false,
+    children: <Page404 />
   }
 ];
