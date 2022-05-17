06a7a32a0e16711d8e2fadb5bdd70bc94005f108
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-05-18 01:51:10 +0900

    useHistoryを使用した画面遷移

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,11 +1,17 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const Page1DetailA = () => {
   const { state } = useLocation();
   console.log(state);
+
+  const history = useHistory();
+
+  const goBackPageA = () => history.goBack();
+
   return (
     <div>
       <h1>Page1DetailAページです。</h1>
+      <button onClick={goBackPageA}>PageAに戻る</button>
     </div>
   );
 };
